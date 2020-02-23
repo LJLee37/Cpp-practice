ecc485d714466e9b0965326203147642fd0a43fc
LJLee37 <oh-my-zsh@kakao.com>
2020-02-23 22:22:40 +0900

    프로토타입 선언/

--- data_structure/list/list.c
+++ data_structure/list/list.c
@@ -2,3 +2,29 @@
 //headers
 #include <stdio.h>
 
+//data types
+
+typedef int Data;
+typedef struct{
+	Data data;
+	Node* next = NULL;
+} Node;
+typedef struct{
+	Node* head;
+	Node* cur;
+
+} List;
+
+//function prototypes
+List* listInit(int data);
+int listDelete(List* target);
+Data listCurData(List* target);
+Data listHead(List* target);
+int listNext(List* target);
+int listInsert(List* tarList, Data tarData);
+int listRemove(List* target);
+
+//function definitions
+int main()
+{
+	//
