4ef0eed716f7675a18e7049db1faf394481883d8
LJLee37 <oh-my-zsh@kakao.com>
2020-03-12 13:43:11 +0900

    컴파일 편의.

---
+++ temp.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int wordAmount = 0;
+vector<string> sort(vector<string> wordList)
+{
+    int newIndex[wordAmount];
+    bool sorted = false;
+    int currentSorted = -1;
+    vector<string> retval;
+    while(!sorted)
+    {
+        char temp1 = 'z';
+        int temptarget = wordList.size();
+        for (int i = 0; i < wordList.size(); i++)
+        {
+            if(temp1 > wordList[i][0])
+            {
+                temp1 = wordList[i][0];
+                temptarget = i;
+            }
+            else if(temp1 == wordList[i][0])
+            {
+                if (temptarget == wordList.size())
+                    temptarget = i;
+                else
+                {
+                    int shorterlen = (wordList[i].length() > wordList[temptarget].length() ) ? wordList[i].length() : wordList[temptarget].length();
+                    for (int j = 1; j < shorterlen; j++)
+                    {
+                        if(wordList[i][j] < wordList[temptarget][j])
+                        {
+                            
+                        }
+                    }
+                }
+                
+            }
+        }
+    }
+}
+
+int main()
+{
+    cin >> wordAmount;
+    vector<string> wordList;
+    string tempString;
+    for (int i = 0; i < wordAmount; i++)
+    {    
+        cin >> tempString;
+        wordList.push_back(tempString);
+    }
+    wordList = sort(wordList)
+    for (int i = 0; i < wordAmount; i++)
+        cout << wordList[i] << endl;
+    return 0;
+}
