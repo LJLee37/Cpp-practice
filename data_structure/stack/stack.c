2e6d81c19cbb23ee934f0ecc23ab9536755d7478
LJLee37 <oh-my-zsh@kakao.com>
2020-03-14 16:08:59 +0900

    스택 구현 완셩.

--- data_structure/stack/stack.c
+++ data_structure/stack/stack.c
@@ -25,7 +25,7 @@ typedef struct stack
 //function prototypes
 Stack* stack_init(Data initData);
 bool stack_push(Stack* target, Data data);
-Data stack_pop(Stack* target);
+void stack_pop(Stack* target);
 Data stack_top(Stack* target);
 unsigned int stack_size(Stack* target);
 bool stack_remove(Stack* target);
@@ -54,19 +54,77 @@ bool stack_push(Stack* target, Data data)
 	tempNode->data = data;
 	tempNode->next = target->top;
 	target->top = tempNode;
+	target->nodeNumber++;
 	return true;
 }
 Data stack_top(Stack* target)
 {
-	return target->top->data;
+	if(target->nodeNumber)
+		return target->top->data;
+	else
+	{
+		printf("Empty\n");
+		return -1;
+	}
+	
 }
-Data stack_pop(Stack* target)
+void stack_pop(Stack* target)
 {
-	Data retval = stack_top(target);
-	NodePtr tempNode = target->top;
-	target->top = tempNode -> next;
-	free(tempNode);
-	target->nodeNumber--;
-	return retval;
+	if(target->nodeNumber)
+	{
+		NodePtr tempNode = target->top;
+		target->top = tempNode -> next;
+		free(tempNode);
+		target->nodeNumber--;
+		return;
+	}
+	else
+	{
+		printf("Warning attemp to pop empty stack\n");
+		printf("Nothing will happen\n");
+		return;
+	}
 }
-
+unsigned int stack_size(Stack* target)
+{
+	return target->nodeNumber;
+}
+int main()
+{
+	printf("스택 테스트용 프로그램.\n");
+	printf("시작 데이터 : ");
+	Data tempData;
+	scanf("%d", &tempData);
+	Stack* stack = stack_init(tempData);
+	while(true)
+	{
+		printf("1. 푸시 2. 팝 3. 탑 4. 사이즈 5. 삭제");
+		printf("선택 : ");
+		unsigned int sw;
+		scanf("%d", &sw);
+		switch (sw)
+		{
+		case 1:
+			printf("푸시할 데이터 입력 : ");
+			scanf("%d", &tempData);
+			stack_push(stack, tempData);
+			break;
+		case 2:
+			printf("팝\n");
+			stack_pop(stack);
+			break;
+		case 3:
+			printf("탑 : %d\n", stack_top(stack));
+			break;
+		case 4:
+			printf("사이즈 : %d\n", stack_size(stack));
+			break;
+		case 5:
+			printf("스택 삭제\n");
+			return (int)(!stack_remove(stack));
+			break;
+		default:
+			break;
+		}
+	}
+}
